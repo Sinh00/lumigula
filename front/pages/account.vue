ea8654ea949b5ee05e8e191b4efb087dc5b691ec
Sinh00 <msoirnthe@gmail.com>
2021-02-16 02:19:59 +0900

    [add] feature#0007 ログイン後のトップページ作成

---
+++ front/pages/account.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <nuxt-child />
+  </v-container>
+</template>
+
+<script>
+export default {
+  layout: 'loggedIn',
+  // /accountというパスが自動生成されるのでアクセス不可に設定
+  validate({ route }) {
+    return route.name !== 'account'
+  },
+}
+</script>
